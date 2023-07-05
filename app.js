adda95ac8c65a2910257c7f1c0ad9229319b6053
sam5102 <ansh102u@gmail.com>
2023-07-05 21:31:41 +0530

    ui updated

--- app.js
+++ app.js
@@ -11,3 +11,41 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden') 
 hiddenElements.forEach((el) => observer.observe(el))
+
+
+const roles = ["UI/UX Designer", "Web Developer", "Freelancer"]
+let text = ""
+let i = 0
+let j = 0
+
+const myDiv = document.getElementById("roles")
+
+function myJobRoles() {
+    const myInterval = setInterval(() => {
+        if(j < roles.length) {
+            const expand = roles[j].split('')
+            if(expand.length-1 >= i) {
+                text += expand[i]
+                i+=1
+                myDiv.innerHTML = text
+            } else {
+                clearInterval(myInterval)
+                text = ""
+                j += 1
+                i = 0
+                myJobRoles()
+            }
+                
+        } else {
+            clearInterval(myInterval)
+            j = 0
+            i = 0
+            myDiv.innerHTML = text
+            myJobRoles()
+        }
+    }, 500)
+} 
+
+setTimeout(() => {
+    myJobRoles();
+}, 1000);
